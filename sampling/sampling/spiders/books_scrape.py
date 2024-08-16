c30e82862666560536536c57df33e5a1936460a3
Pra5etya <careermagic780@gmail.com>
2024-08-16 07:54:25 +0700

    mod

--- sampling/sampling/spiders/books_scrape.py
+++ sampling/sampling/spiders/books_scrape.py
@@ -18,9 +18,10 @@ class CrawlingSpider(CrawlSpider):
 
     # scraping sections
     def parse_item(self, response): 
-        scrape_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+        scrape_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         yield {
+            'url': response.url,
             'title': response.css('.product_main h1::text').get(), 
             'price': response.css('.price_color::text').get(), 
             'availability': response.css('.availability::text')[1].get().replace('\n', '').replace(' ', ''), 
